2be9c160d9cc1ebfe39e39695de274e12eaa5af4
Dinesh Kumar <179027812+dineshkumar8700@users.noreply.github.com>
2025-12-28 12:28:17 +0530

    Added add external command to get sum of numbers

--- commands/externalCommand.js
+++ commands/externalCommand.js
@@ -11,7 +11,19 @@ const ls = () => {
   console.log("ls command executed");
 };
 
+const add = (args) => {
+  try {
+    const sum = args.map((num) => parseInt(num)).reduce((sum, num) =>
+      sum + num
+    );
+    console.log(sum);
+  } catch (error) {
+    console.log("Give at least 1 number");
+  }
+};
+
 export const externalCommands = {
   cat,
   ls,
+  add,
 };
